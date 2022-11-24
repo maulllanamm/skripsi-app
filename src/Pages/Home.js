49842aeb67ab2e8ad1c306aca134b07c432b7a89
maulllanamm <maulllanamuhammad@gmail.com>
2022-11-24 14:17:14 +0700

    pages

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -23,13 +23,13 @@ export default function Home() {
     <div className="home-container">
       <div className="home-parent">
         <div className="home-banner">
-          <img src={Banner} />
+          <img src={Banner} alt="banner" />
         </div>
         <div className="home-upload">
           <div className="home-upload__title">Aplikasi Cek Keaslian Uang</div>
           <div className="home-example">
             {uang.map((el) => {
-              return <img src={el} />;
+              return <img src={el} key={el} alt="img-uang" />;
             })}
           </div>
           <UploadFile />
